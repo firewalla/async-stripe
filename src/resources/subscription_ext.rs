c4c67b0a65bdcd2411b6306b28835b9b40564d13
Ye Xia <only.one.truth.1102@gmail.com>
2024-04-02 11:03:48 +0800

    add new cancel api

--- src/resources/subscription_ext.rs
+++ src/resources/subscription_ext.rs
@@ -10,6 +10,15 @@ pub struct CancelSubscription {
     pub at_period_end: Option<bool>,
 }
 
+#[derive(Clone, Debug, Default, Serialize)]
+pub struct CancelSubscriptionNew {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub prorate: Option<bool>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub invoice_now: Option<bool>,
+}
+
 impl CancelSubscription {
     pub fn new() -> CancelSubscription {
         CancelSubscription { at_period_end: None }
@@ -27,6 +36,15 @@ impl Subscription {
     ) -> Response<Subscription> {
         client.delete_query(&format!("/subscriptions/{}", subscription_id), params)
     }
+
+    pub fn cancel_new(
+        client: &Client,
+        subscription_id: &SubscriptionId,
+        params: CancelSubscriptionNew,
+    ) -> Response<Subscription> {
+        client.delete_query(&format!("/subscriptions/{}", subscription_id), params)
+    }
+
 }
 
 impl CreateSubscriptionItems {
